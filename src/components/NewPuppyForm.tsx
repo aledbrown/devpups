39da7e819aab2dbc572203b9229000128edb29b8
Aled Brown <690500+aledbrown@users.noreply.github.com>
2025-04-18 17:42:46 +0100

    Grab "formData" and log it.

--- src/components/NewPuppyForm.tsx
+++ src/components/NewPuppyForm.tsx
@@ -1,7 +1,16 @@
 export function  NewPuppyForm() {
   return (
     <div className="mt-12 flex items-center justify-between bg-white p-8 shadow ring ring-black/5">
-      <form className="mt-4 flex w-full flex-col items-start gap-4">
+      <form 
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          const name = formData.get('name') as string;
+          const trait = formData.get('trait') as string;
+          console.log({name, trait});
+        }}
+        className="mt-4 flex w-full flex-col items-start gap-4"
+      >
         <div className="grid w-full gap-6 md:grid-cols-3">
           <fieldset className="flex w-full flex-col gap-1">
             <label htmlFor="name">Name</label>
